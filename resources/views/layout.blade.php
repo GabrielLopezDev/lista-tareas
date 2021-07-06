f975146e34249147895cf89b5599403526b859d0
Gabriel López <gabriellopez.dev@gmail.com>
2021-07-06 13:52:29 -0400

    Commit inicial - instalación de Laravel, Livewie, Bootstrap

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Livewire</title>
+
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+        @livewireStyles
+    </head>
+    <body class="antialiased">
+        @yield('content')
+
+        @livewireScripts
+    </body>
+</html>
