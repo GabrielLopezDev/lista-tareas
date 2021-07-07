e61faa10aef989a27ce0e1ea864de26f94cf9462
Gabriel López <gabriellopez.dev@gmail.com>
2021-07-07 14:02:50 -0400

    [livewire] agregar funcionalidad para editar un post

---
+++ resources/views/livewire/edit.blade.php
@@ -0,0 +1,11 @@
+<h2>Editar Post</h2>
+
+@include('livewire.form')
+
+<button wire:click="update" class="btn btn-sm btn-primary mt-2">
+	Actualizar
+</button>
+
+<button wire:click="default" class="btn btn-sm btn-outline-secondary mt-2">
+	Cancelar
+</button>
--- resources/views/livewire/table.blade.php
+++ resources/views/livewire/table.blade.php
@@ -16,7 +16,7 @@
 				<td>{{ $post->title }}</td>
 				<td>{{ $post->body }}</td>
 				<td>
-					<button class="btn btn-sm btn-primary">
+					<button wire:click="edit({{ $post->id }})" class="btn btn-sm btn-primary">
 						Editar
 					</button>
 				</td>
